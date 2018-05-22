d310bbe82928fdd73de93fedafc035861ef62d00
xtrafrancyz <xtrafrancyz@gmail.com>
2018-05-22 17:07:36 +0300

    Исправлены значения по умолчанию

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
 	var webHost = flag.String("webhost", "0.0.0.0:7424", "address to bind webserver")
-	var confirmationKey = flag.String("confirmation-key", "0.0.0.0:7424", "confirmation key from vk")
-	var secretKey = flag.String("secret-key", "0.0.0.0:7424", "secret key from vk")
+	var confirmationKey = flag.String("confirmation-key", "123qwe", "confirmation key from vk")
+	var secretKey = flag.String("secret-key", "123qwe", "secret key from vk")
 	var influxUrl = flag.String("influx-url", "http://127.0.0.1:8086", "address of InfluxDB")
-	var influxDatabase = flag.String("influx-database", "http://127.0.0.1:8086", "database name")
-	var influxRetentionPolicy = flag.String("influx-rp", "a_day", "retention policy")
+	var influxDatabase = flag.String("influx-database", "vk_graphs", "database name")
+	var influxRetentionPolicy = flag.String("influx-rp", "a_year", "retention policy")
 
 	flag.Parse()
 
